03fb8f7beae9a11993f83c0bb47bdad5c8a0209a
Mayuri Wasu <93375338+mayuriwasu1@users.noreply.github.com>
2022-03-06 16:00:43 +0530

    files added

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const container = document.querySelector('.grid-scroll');
+var page = 1; 
+// default img
+const loadImage = async () => {
+    let res = await fetch(
+      `https://api.unsplash.com/photos/?client_id=xcUUJf1lrIO_d5FCizTpQQZjBWL2Pa8Q8-aWSPiuzdQ&per_page=15&page=${page}`
+    );
+    let data = await res.json();
+    showImage(data);
+  };
+  loadImage();
+// info of default
+function showImage(items) {
+    items.forEach((item) => {
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = item.urls.small;
+    //   let title = document.createElement("p");
+    //   title.innerText = item.description;
+    //   title.style.color = "#ffffff";
+    //   title.style.fontSize = "1.4vw";
+      div.append(img);
+      container.insertAdjacentElement("beforeend", div);
+    });
+  }
+//timeout 
+function showData(){
+    setTimeout(()=>{
+        page++;
+        loadImage();
+    }, 200)
+}
+window.addEventListener('scroll', ()=>{
+    //default 
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+    if(scrollTop+clientHeight >= scrollHeight){
+        showData()
+    }
+
+})
